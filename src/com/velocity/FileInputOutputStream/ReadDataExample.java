e8a60ea95d10943f87cd742da9c3a621f2af5fe1
Shilpa Mali <malishilpa037@gmail.com>
2024-12-28 10:47:53 +0530

    File Input Output Stream

---
+++ src/com/velocity/FileInputOutputStream/ReadDataExample.java
@@ -0,0 +1,41 @@
+package com.velocity.FileInputOutputStream;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Scanner;
+
+//write a java program to read the data from file
+public class ReadDataExample {
+
+	public static void main(String[] args) throws IOException {
+
+		/*
+		 * int a;  //declaration
+		 * a=50; //initialization
+		 */
+		FileInputStream fis = null;
+		Scanner scanner = null;
+		try {
+			// Step-1- Create object of FileInputStream and pass filepath
+			fis = new FileInputStream("C:\\Users\\SHILPA\\OneDrive\\Desktop");
+			// Step-2- create the object of Scanner class and pass fis object
+			scanner = new Scanner(fis); // I am software engineer pune
+			// Step-3- while loop
+			while (scanner.hasNextLine()) {
+				// hasNextLine() method return boolean value- true
+				// String s = scanner.nextLine();
+				// System.out.println(s);
+				// Step-4- print data
+				System.out.println(scanner.nextLine());
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			//we always close connection resource in finally block 
+			//we can close in try block but that is bad coding practice.
+			fis.close();
+			scanner.close();
+		}
+
+	}
+}
---
+++ src/com/velocity/FileInputOutputStream/WriteDataExample.java
@@ -0,0 +1,21 @@
+package com.velocity.FileInputOutputStream;
+
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class WriteDataExample {
+
+	public static void main(String[] args) throws IOException {
+
+		// FileOutputStream fos = null;
+		// Step-1- Create object of FileWriter
+		FileWriter fWriter = new FileWriter("C:\\Users\\SHILPA\\OneDrive\\Desktop");
+		// Step-2- use write() method
+		//scanner-> nextLine() method-String s
+		fWriter.write("This is online java classes...");
+		//fWriter.close();
+		// Step-3- meaningful message
+		System.out.println("File written successfully..");
+
+	}
+}
